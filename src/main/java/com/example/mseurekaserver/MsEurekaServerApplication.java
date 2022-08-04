1b5f1fd9ae1217a31a483aa440d2f11b1db74692
alisson <arteaga10alisson@gmail.com>
2022-08-04 12:19:12 -0500

    refactor

--- src/main/java/com/example/mseurekaserver/MsEurekaServerApplication.java
+++ src/main/java/com/example/mseurekaserver/MsEurekaServerApplication.java
@@ -1,14 +1,20 @@
-package com.example.ms_eureka_server;
+package com.example.mseurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Eureka Server application.
+ *
+ * @author Alisson Arteaga / Christian Dionisio
+ * @version 1.0
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class MsEurekaServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MsEurekaServerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MsEurekaServerApplication.class, args);
+  }
 
 }
--- src/test/java/com/example/mseurekaserver/MsEurekaServerApplicationTests.java
+++ src/test/java/com/example/mseurekaserver/MsEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ms_eureka_server;
+package com.example.mseurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
